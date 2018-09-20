de1e88cc6cfd7f8c59f79f2bc0754999c8324563
Rockheung <rockheung@gmail.com>
2018-09-20 22:24:53 +0900

    update model

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Work(models.Model):
+    '''A model for each story'''
+    user = models.ForeignKey(User,
+                             on_delete=models.SET_NULL,
+                             null=True)
+    title = models.CharField(max_length=255)
+    detail = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Episode(models.Model):
+    user = models.ForeignKey(User,
+                             on_delete=models.SET_NULL,
+                             null=True)
+    work = models.ForeignKey(Work,
+                             on_delete=models.SET_NULL,
+                             null=True)
+    title = CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Cut(models.Model):
+    index = models.PositiveIntegerField(primary_key=True)
+    episode = models.ForeignKey(Episode,
+                                on_delete=models.CASCADE)
+    x = models.IntegerField()
+    y = models.IntegerField()
+    width = models.IntegerField()
+    height = models.IntegerField()
+    x_canvas = model.IntegerField()
+    y_canvas = model.IntegerField()
+
+    def __str__(self):
+        return self.index
+
+
+class Cluster(models.Model):
+    work = models.ForeignKey(Work,
+                             models.SET_NULL,
+                             null=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Instance(models.Model):
+    index = models.PositiveIntegerField(primary_key=True)
+    cut = models.ForeignKey(Cut,
+                            on_delete=models.CASCADE)
+    cluster = models.ForeignKey(Cluster,
+                                on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.index
+
+
+class KeyPoint(models.Model):
+    index = models.PositiveIntegerField(primary_key=True)
+    # nose, left_eye, right_eye, left_ear, right_ear, left_shoulder, right_shoulder,
+    # left_elbow, right_elbow, left_wrist, right_wrist, left_hip, right_hip, left_knee,
+    # right_knee, left_ankle, right_ankle
+    # So, 0 to 16, zero: nose,  odd: left, even: right
+    partnum = models.PositiveSmallIntegerField()
+    x = models.IntegerField()
+    y = models.IntegerField()
+    # 0: null, 1: obscure, 2: see
+    visible = models.NullBooleanField()
+
+    def __str__(self):
+        return self.index
---
+++ webapp/static/webapp/js/main.js
@@ -0,0 +1,24 @@
+(function () {
+  var PSD = require('psd');
+
+  document.getElementById('dropzone').addEventListener('dragover', onDragOver, true);
+  document.getElementById('dropzone').addEventListener('drop', onDrop, true);
+
+  function onDragOver(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
+  }
+
+  function onDrop(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    PSD.fromEvent(e).then(function (psd) {
+      var data = JSON.stringify(psd.tree().export(), undefined, 2);
+      document.getElementById('data').innerHTML = data;
+      document.getElementById('image').appendChild(psd.image.toPng());
+    });
+  }
+}());
+
